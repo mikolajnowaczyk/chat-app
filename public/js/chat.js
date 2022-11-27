06592619ce93e5ec9a3e6d59ca8385ad30c23b34
Mikolaj <mikolaj35@wp.pl>
2022-11-27 16:51:05 +0100

    Formatting and send simple message functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,16 @@
-io()
+const socket = io();
+
+socket.on("message", (msg) => {
+  console.log(msg);
+});
+
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const clientMsg = e.target.elements.message.value;
+
+  socket.emit("sendMessage", clientMsg, (message) => {
+    console.log("The message was delivered", message);
+  });
+});
+document.querySelector("#send-location").addEventListener("click", () => {
--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
-const path = require('path')
-const express = require('express')
-const http = require('http')
-const socketio = require('socket.io')
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, "../public")
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', ()=>{
-  console.log("New WebSocket connection"); 
-})
+io.on("connection", (socket) => {
+  socket.emit("message", "Welcome");
+  socket.broadcast.emit("message", "A new user has joined!");
+  
+  socket.on("sendMessage", (msg, callback)=>{
+    io.emit("message", msg);
+    callback("yes.");
+  })
+
+  socket.on('disconnect', ()=>{
+    io.emit('message', 'A user has left');
+  })
+});
+
 
 
 server.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
